9dca438817a062c96b48c2fd35a2af499b0724a0
ardakuvanc3 <ardakuvanc3@gmail.com>
2024-07-15 17:25:28 +0300

    Bloc Pattern

--- lib/ui/views/anasayfa.dart
+++ lib/ui/views/anasayfa.dart
@@ -1,6 +1,8 @@
 import 'package:filmler_app/data/entity/filmler.dart';
+import 'package:filmler_app/ui/cubit/anasayfa_cubit.dart';
 import 'package:filmler_app/ui/views/detay_sayfa.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AnaSayfa extends StatefulWidget {
   const AnaSayfa({super.key});
@@ -11,59 +13,47 @@ class AnaSayfa extends StatefulWidget {
 
 class _AnaSayfaState extends State<AnaSayfa> {
 
-
-  Future<List<Filmler>> filmleriYukle() async {
-    var filmlerListesi = <Filmler>[];
-    var f1 = Filmler(id: 1, ad: "Django", resim: "django.png", fiyat: 24);
-    var f2 = Filmler(id: 2, ad: "Interstellar", resim: "interstellar.png", fiyat: 32);
-    var f3 = Filmler(id: 3, ad: "Inception", resim: "inception.png", fiyat: 16);
-    var f4 = Filmler(id: 4, ad: "The Hateful Eight", resim: "thehatefuleight.png", fiyat: 28);
-    var f5 = Filmler(id: 5, ad: "The Pianist", resim: "thepianist.png", fiyat: 18);
-    var f6 = Filmler(id: 6, ad: "Anadoluda", resim: "anadoluda.png", fiyat: 10);
-    filmlerListesi.add(f1);
-    filmlerListesi.add(f2);
-    filmlerListesi.add(f3);
-    filmlerListesi.add(f4);
-    filmlerListesi.add(f5);
-    filmlerListesi.add(f6);
-    return filmlerListesi;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    context.read<AnasayfaCubit>().filmleriYukle();
   }
 
-@override
+
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Filmler"),),
-      body: FutureBuilder<List<Filmler>>(
-        future: filmleriYukle(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData){
-            var filmlerListesi = snapshot.data;
-            return GridView.builder(
-              itemCount: filmlerListesi!.length,
+      body: BlocBuilder<AnasayfaCubit,List<Filmler>>(
+        builder: (context, filmlerListesi) {
+          if (filmlerListesi.isNotEmpty){return GridView.builder(
+              itemCount: filmlerListesi.length,
               gridDelegate:
               const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2, childAspectRatio: 1 / 1.7),
               itemBuilder: (context, indeks) {
-                  var film = filmlerListesi[indeks];
-                  return GestureDetector(
-                    onTap: () {
-                      Navigator.push(context, MaterialPageRoute(builder: (context) => DetaySayfa(film: film)));
-                    },
-                    child: Card(
-                      child: Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Image.asset("resimler/${film.resim}"),
-                          Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              Text("${film.fiyat} ₺ ", style: TextStyle(fontSize: 24),),
-                              ElevatedButton(onPressed: () {
-                                print("${film.ad} sepete eklendi");
-                              }, child: const Text("Sepete Ekle")),
-                            ],
-                          )
-                        ],
-                      ),
+                var film = filmlerListesi[indeks];
+                return GestureDetector(
+                  onTap: () {
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => DetaySayfa(film: film)));
+                  },
+                  child: Card(
+                    child: Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Image.asset("resimler/${film.resim}"),
+                        Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Text("${film.fiyat} ₺ ", style: TextStyle(fontSize: 24),),
+                            ElevatedButton(onPressed: () {
+                              print("${film.ad} sepete eklendi");
+                            }, child: const Text("Sepete Ekle")),
+                          ],
+                        )
+                      ],
                     ),
-                  );
+                  ),
+                );
               },
             );
           }
